SmPMODXL/android_packages_services_Telephony

Telephony: reload CarrierConfig based on changes to ro.purenexus.version
PureNexus overrides the build fingerprint with a value from the factory images, which means this check will never change even if the user dirty flashes a new build

src/com/android/phone/CarrierConfigLoader.java
@@ -316,11 +316,11 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
                     SharedPreferences sharedPrefs =
                             PreferenceManager.getDefaultSharedPreferences(mContext);
                     final String lastFingerprint = sharedPrefs.getString(KEY_FINGERPRINT, null);
-                    if (!Build.FINGERPRINT.equals(lastFingerprint)) {
+                    if (!Build.NEXUS.equals(lastFingerprint)) {
                         log("Build fingerprint changed. old: "
-                                + lastFingerprint + " new: " + Build.FINGERPRINT);
+                                + lastFingerprint + " new: " + Build.NEXUS);
                         clearCachedConfigForPackage(null);
-                        sharedPrefs.edit().putString(KEY_FINGERPRINT, Build.FINGERPRINT).apply();
+                        sharedPrefs.edit().putString(KEY_FINGERPRINT, Build.NEXUS).apply();
                     }
                     break;
             }

ezio84/abc_packages_services_Telephony

src/com/android/phone/CarrierConfigLoader.java
@@ -328,11 +328,11 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
                     SharedPreferences sharedPrefs =
                             PreferenceManager.getDefaultSharedPreferences(mContext);
                     final String lastFingerprint = sharedPrefs.getString(KEY_FINGERPRINT, null);
-                    if (!Build.FINGERPRINT.equals(lastFingerprint)) {
+                    if (!Build.DATE.equals(lastFingerprint)) {
                         log("Build fingerprint changed. old: "
-                                + lastFingerprint + " new: " + Build.FINGERPRINT);
+                                + lastFingerprint + " new: " + Build.DATE);
                         clearCachedConfigForPackage(null);
-                        sharedPrefs.edit().putString(KEY_FINGERPRINT, Build.FINGERPRINT).apply();
+                        sharedPrefs.edit().putString(KEY_FINGERPRINT, Build.DATE).apply();
                     }
                     break;
             }